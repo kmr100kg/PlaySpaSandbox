9813c3e16014b9dea7355fb2311016137741ee25
kmr100kg <kmr100kg@gmail.com>
2018-05-19 12:57:30 +0900

    [Fix] SQLを修正し忘れれた

--- sample.sql
+++ sample.sql
@@ -13,13 +13,13 @@ create table employee(
   update_date timestamp not null
 );
 
-insert into employee values(1, 'サンプル一郎', 'サンプルイチロウ', 'sample1@mail.co.jp', 'test1234');
-insert into employee values(2, 'サンプル二郎', 'サンプルジロウ', 'sample2@mail.co.jp', 'test1234');
-insert into employee values(3, 'サンプル三郎', 'サンプルサブロウ', 'sample3@mail.co.jp', 'test1234');
-insert into employee values(4, 'サンプル四郎', 'サンプルシロウ', 'sample4@mail.co.jp', 'test1234');
-insert into employee values(5, 'サンプル五郎', 'サンプルゴロウ', 'sample5@mail.co.jp', 'test1234');
-insert into employee values(6, 'サンプル六郎', 'サンプルロクロウ', 'sample6@mail.co.jp', 'test1234');
-insert into employee values(7, 'サンプル七郎', 'サンプルナナロウ', 'sample7@mail.co.jp', 'test1234');
-insert into employee values(8, 'サンプル八郎', 'サンプルハチロウ', 'sample8@mail.co.jp', 'test1234');
-insert into employee values(9, 'サンプル九郎', 'サンプルクロウ', 'sample9@mail.co.jp', 'test1234');
-insert into employee values(10, 'サンプル十郎', 'サンプルジュウロウ', 'sample10@mail.co.jp', 'test1234');
+insert into employee values(1, 'サンプル一郎', 'サンプルイチロウ', 'sample1@mail.co.jp', 'test1234', now());
+insert into employee values(2, 'サンプル二郎', 'サンプルジロウ', 'sample2@mail.co.jp', 'test1234', now());
+insert into employee values(3, 'サンプル三郎', 'サンプルサブロウ', 'sample3@mail.co.jp', 'test1234', now());
+insert into employee values(4, 'サンプル四郎', 'サンプルシロウ', 'sample4@mail.co.jp', 'test1234', now());
+insert into employee values(5, 'サンプル五郎', 'サンプルゴロウ', 'sample5@mail.co.jp', 'test1234', now());
+insert into employee values(6, 'サンプル六郎', 'サンプルロクロウ', 'sample6@mail.co.jp', 'test1234', now());
+insert into employee values(7, 'サンプル七郎', 'サンプルナナロウ', 'sample7@mail.co.jp', 'test1234', now());
+insert into employee values(8, 'サンプル八郎', 'サンプルハチロウ', 'sample8@mail.co.jp', 'test1234', now());
+insert into employee values(9, 'サンプル九郎', 'サンプルクロウ', 'sample9@mail.co.jp', 'test1234', now());
+insert into employee values(10, 'サンプル十郎', 'サンプルジュウロウ', 'sample10@mail.co.jp', 'test1234', now());
